8c01abb2b5f581a446447a8096df8dff2ca5eb9d
Olena Hulakova <ohulakova@gmail.com>
2019-11-12 19:31:16 +0000

    Update display value

--- calc.js
+++ calc.js
@@ -14,13 +14,28 @@ var clearBtn = document.getElementById('calc-clear');
 var backspaceBtn = document.getElementById('calc-backspace');
 var displayValElement = document.getElementById('calc-display-val');
 
+var displayVal = '0';
+var pendingVal;
+var evalStringArray = [];
+
 var calcNumBtns = document.getElementsByClassName('calc-btn-num');
 var calcOperatorBtns = document.getElementsByClassName('calc-btn-operator');
+
+var updateDisplayVal = (clickObj) => {
+    var btnText = clickObj.target.innerText;
+
+    if (displayVal === '0')
+        displayVal = '';
+
+    displayVal += btnText;
+    displayValElement.innerText = displayVal;
+}
+
 
 for (let i = 0; i < calcNumBtns.length; i++) {
    calcNumBtns[i].addEventListener('click', updateDisplayVal, false)
 }
-
-for (let i = 0; i < calcOperatorBtns.length; i++) {
-   calcOperatorBtns[i].addEventListener('click', perfomOperation, false)
-}
+//
+// for (let i = 0; i < calcOperatorBtns.length; i++) {
+//    calcOperatorBtns[i].addEventListener('click', perfomOperation, false)
+// }
